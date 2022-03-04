dcd737f744e9b48ec266dfbf08f5e9909e34e331
l3th4l <safalyaglobal@gmail.com>
2022-03-05 02:59:54 +0530

    LCG

--- gen.py
+++ gen.py
@@ -2,18 +2,19 @@ import time
 
 from matplotlib import pyplot as plt 
 
-seed = time.time()%(10**5)
+seed = int(time.time()*time.time_ns())%(10**5)
+print(seed)
 
 x = []
 
-for i in range(500):
+for i in range(10):
 
     seed = int(str(seed**2)[:5])
 
     rn = int(str(seed)[1:3])
     x.append(rn)
 
-    print(rn)
+print(x)
 
 plt.hist(x)
 plt.show()
